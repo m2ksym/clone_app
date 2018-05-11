960b4962e3951709f15e71e63d895dd54002704c
Mizuki <mizu9729@outlook.jp>
2018-05-11 12:16:08 +0900

    Initialized repository

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
+end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  
+
+end
